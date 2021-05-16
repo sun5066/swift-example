417b14db8f64970630914be4cc3f6399854fe3ff
sun5066 <alstjr4434@naver.com>
2021-05-17 00:04:47 +0900

    init playground res

---
+++ playground/playground_res.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let cgRect = CGRect(x: 0, y: 0, width: 200, height: 50)
+let label = UILabel(frame: cgRect)
+label.text = "반갑다 그래."
+label.backgroundColor = .red
+label.textAlignment = .center
+label.font = UIFont(name: "맑은 고딕", size: 30)
+label
+
+let swiftLogo = UIImage(named: "logo")
